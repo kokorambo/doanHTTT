638e1cd3b3c7df9c3b6e21a051ee2efb2e9bef60
lehongduy <duylhhvcs@gmail.com>
2015-10-31 21:19:53 +0700

    Update xemdiemthi.php

--- hethongxemdiemthi/views/xemdiemthi.php
+++ hethongxemdiemthi/views/xemdiemthi.php
@@ -1,3 +1,44 @@
-<?php
 
-echo'Đây là trang xem điểm thi';
+<!DOCTYPE html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Nộp hồ sơ</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="diemthi1">
+                    <div class="head">
+                        <h2>Xem điểm thi tốt nghiệp THPT 2016</h2>
+                    </div>
+                    
+                                <div class="diemthi2">
+                                    <label class="form-inline">Chọn tỉnh thành</label>
+                                </div>
+                                <select name="quequan" class="form-control">
+                                          
+                                </select>
+                            </div>
+                            <div class="diemthi2">
+                                <div class="col-xs-5">
+                                    <label class="form-inline">Chọn cụm thi</label>
+                                </div>
+                            <select name="cumthi" class="form-control">
+                              
+                            </select>  
+                            </div>
+              <div class="form-group">
+                                <div class="diemthi2">
+                                    <label class="form-inline">SBD hoặc Tên</label>
+                                </div>
+                                <input type="text" name="SBD" class="form-control"><br>                     
+                                <input type="submit"  name="ketqua" value="Xem kết quả" class="btnketqua " style="margin-left: 0px">
+                            </div>
+                        </form>
+                    </div>  
+                </div>
+            </div>
+        </div>
+  
+</body>
+</html>
